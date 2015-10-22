d18f361474e424c6bd1916252fda05c219a212c8
Morthalin <morthalin@gmail.com>
2015-10-22 12:27:42 +0200

    [Scripts]Uprava movement scriptu + pridany script na ovladanie kamery.

---
+++ V pasti/Assets/Scripts/Controllers/FollowTrackingCamera.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTrackingCamera : MonoBehaviour
+{
+    public Transform target;
+    
+    public float height = 20f;
+    public float distance = 20f;
+    public float minZoom = 10f;
+    public float maxZoom = 60f;
+    public float minHeight = 10f;
+    public float maxHeight = 60f;
+    public float rotateSpeedX = 1f;
+    public float rotateSpeedY = 100f;
+    public bool doRotate;
+    public bool doZoom;
+    public float zoomStep = 15f;
+    public float zoomSpeed = 5f;
+    private float heightWanted;
+    private float distanceWanted;
+    
+    private Vector3 zoomResult;
+    private Quaternion rotationResult;
+    private Vector3 targetAdjustedPosition;
+
+    void Start()
+    {
+        heightWanted = height;
+        distanceWanted = distance;
+        zoomResult = new Vector3(0f, height, -distance);
+    }
+
+    void LateUpdate()
+    {
+        if (!target)
+        {
+            Debug.LogError("This camera has no target, you need to assign a target in the inspector.");
+            return;
+        }
+
+        // Zoom a posun po Y ose
+        if (doZoom)
+        {
+            // Nacitanie polohy mysky
+            heightWanted -= Input.GetAxis("Mouse Y") * rotateSpeedY * 0.01f;
+            distanceWanted -= Input.GetAxis("Mouse ScrollWheel") * zoomStep;
+
+            // Orezanie na limit
+            heightWanted = Mathf.Clamp(heightWanted, minHeight, maxHeight);
+            distanceWanted = Mathf.Clamp(distanceWanted, minZoom, maxZoom);
+            height = Mathf.Lerp(height, heightWanted, Time.deltaTime * zoomSpeed);
+            distance = Mathf.Lerp(distance, distanceWanted, Time.deltaTime * zoomSpeed);
+
+            // Poslanie vysledku
+            zoomResult = new Vector3(0f, height, -distance);
+        }
+
+        // Rotacia po X
+        if (doRotate)
+        {
+            float currentRotationAngle = transform.eulerAngles.y;
+            float wantedRotationAngle = target.eulerAngles.y;
+
+            // Zjemnenie rotacie
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotateSpeedX * Time.deltaTime);
+
+            // Poslanie vysledku
+            rotationResult = Quaternion.Euler(0f, currentRotationAngle, 0f);
+        }
+
+        // Nastavenie posunu
+        targetAdjustedPosition = rotationResult * zoomResult;
+        transform.position = target.position + targetAdjustedPosition;
+        
+        // Pohlad na target
+        transform.LookAt(target);
+    }
+}
