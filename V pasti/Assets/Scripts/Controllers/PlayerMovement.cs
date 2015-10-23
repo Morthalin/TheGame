11c238cb2fe9728c1e1d4dbf4534567d10e42393
Morthalin <morthalin@gmail.com>
2015-10-23 18:42:38 +0200

    [Animations][Scripts] Pridane animace, animator a script na animace.

--- V pasti/Assets/Scripts/Controllers/PlayerMovement.cs
+++ V pasti/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -7,19 +7,25 @@ public class PlayerMovement : MonoBehaviour
     public float rotateSpeed;
     private Vector3 movementVector;
     private Vector3 rotateVector;
+    private float horizontal;
+    private float vertical;
 
     private CharacterController control;
+    private Animator animator;
 
     void Start ()
     {
         control = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-	void Update ()
+    void Update()
     {
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
         //Movement
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
         movementVector = new Vector3(horizontal, 0f, vertical);
         movementVector.Normalize();
         movementVector = transform.TransformDirection(movementVector);
@@ -29,5 +35,76 @@ public class PlayerMovement : MonoBehaviour
         //Rotation
         rotateVector = new Vector3(0f, Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime, 0f);
         transform.Rotate(rotateVector);
+
+        Animate(horizontal, vertical);
+    }
+
+    void Animate (float horizontal, float vertical)
+    {
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("runningForward", false);
+            animator.SetBool("runningBack", false);
+            animator.SetBool("runningLeft", false);
+            animator.SetBool("runningRight", false);
+            animator.SetBool("runningForwardLeft", false);
+            animator.SetBool("runningForwardRight", false);
+            animator.SetBool("runningBackLeft", false);
+            animator.SetBool("runningBackRight", false);
+        }
+        else
+        {
+            animator.SetInteger("idleState", Random.Range(1, 30));
+            animator.SetBool("isRunning", false);
+            animator.SetBool("runningForward", false);
+            animator.SetBool("runningBack", false);
+            animator.SetBool("runningLeft", false);
+            animator.SetBool("runningRight", false);
+            animator.SetBool("runningForwardLeft", false);
+            animator.SetBool("runningForwardRight", false);
+            animator.SetBool("runningBackLeft", false);
+            animator.SetBool("runningBackRight", false);
+            return;
+        }
+
+        if(vertical > 0)
+        {
+            if(horizontal == 0)
+            {
+                animator.SetBool("runningForward", true);
+            }
+            else if(horizontal < 0)
+            {
+                animator.SetBool("runningForwardLeft", true);
+            }
+            else if (horizontal > 0)
+            {
+                animator.SetBool("runningForwardRight", true);
+            }
+        }
+        else if(vertical < 0)
+        {
+            if (horizontal == 0)
+            {
+                animator.SetBool("runningBack", true);
+            }
+            else if (horizontal < 0)
+            {
+                animator.SetBool("runningBackLeft", true);
+            }
+            else if (horizontal > 0)
+            {
+                animator.SetBool("runningBackRight", true);
+            }
+        }
+        else if(horizontal < 0)
+        {
+            animator.SetBool("runningLeft", true);
+        }
+        else if(horizontal > 0)
+        {
+            animator.SetBool("runningRight", true);
+        }
     }
 }
