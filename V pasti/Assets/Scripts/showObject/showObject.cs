364cd38a7184f18b9497c19ee503ed51d8c82a5e
Chariphuk <Chariphuk@gmail.com>
2015-11-28 09:25:03 +0100

    [npcs]

---
+++ V pasti/Assets/Scripts/showObject/showObject.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(SphereCollider))]
+
+
+public class showObject : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject NPCs;
+	public GameObject items;
+	SphereCollider sc;
+	void Start () {
+		sc = GetComponent<SphereCollider>();
+		sc.isTrigger = true;
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (NPCs) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Exit");
+				NPCs.SetActive (false);
+			}
+		}
+		if (items) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Exit");
+				NPCs.SetActive (false);
+			}
+		}
+	}
+
+	void OnTriggerStay (Collider other) {
+		if (NPCs) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Enter");
+				NPCs.SetActive (true);
+			}
+		}
+		if (items) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Enter");
+				NPCs.SetActive (true);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (NPCs) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Enter");
+				NPCs.SetActive (true);
+			}
+		}
+		if (items) {
+			if (other.gameObject.name == "Player") {
+				Debug.Log ("Enter");
+				NPCs.SetActive (true);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
