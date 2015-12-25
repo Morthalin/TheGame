3bdebd70285c9842a237261fbb18b1aa76f9e28d
Morthalin <morthalin@gmail.com>
2015-12-25 23:46:18 +0100

    [Scripts] Pridana druha cast eventu.

---
+++ V pasti/Assets/Scripts/SpecialEvent/CheckpointStarter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointStarter : MonoBehaviour
+{
+    public GameObject goblin1;
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.name == "Player" && transform.parent.GetComponent<EventController>().checkpoint == 3)
+        {
+            transform.parent.GetComponent<EventController>().checkpoint++;
+        }
+
+        if(collider.gameObject.name == "Goblin" && transform.parent.GetComponent<EventController>().checkpoint == 2)
+        {
+            collider.gameObject.SetActive(false);
+            goblin1.SetActive(true);
+            transform.parent.GetComponent<EventController>().checkpoint++;
+        }
+    }   
+}
--- V pasti/Assets/Scripts/SpecialEvent/EventController.cs
+++ V pasti/Assets/Scripts/SpecialEvent/EventController.cs
@@ -7,6 +7,7 @@ public class EventController : MonoBehaviour
     public int checkpoint;
     public Transform player;
     public Transform goblin;
+    public Transform goblin1;
     public Transform mainCamera;
 
     private bool cameraMove;
@@ -24,6 +25,11 @@ public class EventController : MonoBehaviour
         if (!goblin)
         {
             Debug.LogError("Missing goblin!");
+        }
+
+        if (!goblin1)
+        {
+            Debug.LogError("Missing goblin1!");
         }
 
         if (!mainCamera)
@@ -63,30 +69,64 @@ public class EventController : MonoBehaviour
                             {
                                 mainCamera.rotation = Quaternion.Lerp(mainCamera.rotation, Quaternion.LookRotation((goblin.position + new Vector3(0f, 5f, 0f)) - mainCamera.position), Time.deltaTime * 20f);
                             }
-                            mainCamera.position = mainCamera.position + mainCamera.TransformDirection(0f, 0f, Time.deltaTime * 20);
+                            mainCamera.position = mainCamera.position + mainCamera.TransformDirection(0f, 0f, Time.deltaTime * 40);
                         }
                         else
                         {
-                            StartCoroutine(cameraWait());
+                            StartCoroutine(cameraWait1());
                             cameraMove = false;
                             checkpoint++;
                         }
                     }
                 }
                 break;
-            case 2:
+            case 4:
+                if (player.GetComponent<BasePlayer>().pause == 0)
+                {
+                    player.GetComponent<BasePlayer>().pause++;
+                    mainCamera.GetComponent<FollowTrackingCamera>().enabled = false;
+                }
+                else
+                {
+                    if (!cameraMove)
+                    {
+                        if (Vector3.Angle((goblin1.position + new Vector3(0f, 5f, 0f)) - mainCamera.position, mainCamera.forward) > 1f)
+                        {
+                            mainCamera.rotation = Quaternion.Lerp(mainCamera.rotation, Quaternion.LookRotation((goblin1.position + new Vector3(0f, 5f, 0f)) - mainCamera.position), Time.deltaTime * 2f);
+                        }
+                        else
+                        {
+                            cameraMove = true;
+                        }
+                    }
+                    else
+                    {
+                        if ((mainCamera.position - (goblin1.position + new Vector3(0f, 5f, 0f))).sqrMagnitude > 65f)
+                        {
+                            if (Vector3.Angle((goblin1.position + new Vector3(0f, 5f, 0f)) - mainCamera.position, mainCamera.forward) > 0f)
+                            {
+                                mainCamera.rotation = Quaternion.Lerp(mainCamera.rotation, Quaternion.LookRotation((goblin1.position + new Vector3(0f, 5f, 0f)) - mainCamera.position), Time.deltaTime * 20f);
+                            }
+                            mainCamera.position = mainCamera.position + mainCamera.TransformDirection(0f, 0f, Time.deltaTime * 60);
+                        }
+                        else
+                        {
+                            StartCoroutine(cameraWait2());
+                            cameraMove = false;
+                            checkpoint++;
+                        }
+                    }
+                }
                 break;
-            case 3:
+            case 6:
                 break;
             default:
                 break;
         }
 	}
 
-    IEnumerator cameraWait()
+    IEnumerator cameraWait1()
     {
-        //mainCamera.LookAt(goblin);
-        yield return new WaitForSeconds(1f);
         goblin.LookAt(player);
         goblin.FindChild("FloatingDamageText").FindChild("Text").GetComponent<Text>().text = "!";
         goblin.FindChild("FloatingDamageText").FindChild("Text").GetComponent<Animator>().SetTrigger("Hit");
@@ -96,6 +136,19 @@ public class EventController : MonoBehaviour
         mainCamera.rotation = mainCamera.parent.rotation;
         mainCamera.GetComponent<FollowTrackingCamera>().enabled = true;
         player.GetComponent<BasePlayer>().pause--;
+    }
 
+    IEnumerator cameraWait2()
+    {
+        //goblin1.LookAt(player);
+        goblin1.FindChild("FloatingDamageText").FindChild("Text").GetComponent<Text>().text = "!";
+        goblin1.FindChild("FloatingDamageText").FindChild("Text").GetComponent<Animator>().SetTrigger("Hit");
+        yield return new WaitForSeconds(0.5f);
+        goblin1.GetComponent<Animator>().SetBool("Attacking", true);
+        yield return new WaitForSeconds(5f);
+        mainCamera.position = mainCamera.parent.position;
+        mainCamera.rotation = mainCamera.parent.rotation;
+        mainCamera.GetComponent<FollowTrackingCamera>().enabled = true;
+        player.GetComponent<BasePlayer>().pause--;
     }
 }
